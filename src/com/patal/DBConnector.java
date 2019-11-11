574d52a7d5961f5da78423c53be0bd2b5747f89c
Patryk Abraś <patrykabras@gmail.com>
2019-11-11 13:18:47 +0100

    DBConntector class done

---
+++ src/com/patal/DBConnector.java
@@ -0,0 +1,33 @@
+package com.patal;
+import java.sql.*;
+
+public class DBConnector {
+    private static Connection con;
+    private static String url = "jdbc:mysql://localhost:3306/xartesus";
+    private static String user = "root";
+    private static String password = "";
+    public DBConnector() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void sendSQL(String sql){
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+//            System.out.println(rs.getString("fName"));
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+
+            con.close();
+        } catch (SQLException  e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/com/patal/LogIn.java
+++ src/com/patal/LogIn.java
@@ -16,7 +16,7 @@ public class LogIn extends HttpServlet {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        dbTest();
+        DBConnector.sendSQL("select * from users");
         String url = "/Hello.jsp";
         String userName = request.getParameter("name");
         request.setAttribute("userName", userName);
@@ -31,26 +31,5 @@ public class LogIn extends HttpServlet {
         doPost(request,response);
     }
 
-    private void dbTest() {
-        try {
-
-
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
-//here sonoo is the database name, root is the username and root is the password
-            Statement stmt = con.createStatement();
-
-            ResultSet rs = stmt.executeQuery("select * from usertest");
-
-//            System.out.println(rs.getString("fName"));
-            while (rs.next())
-                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-
-            con.close();
-        } catch (SQLException  e) {
-            e.printStackTrace();
-
-
-        }
-
-    }
 }
+
